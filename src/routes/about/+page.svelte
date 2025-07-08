717b2bb70e72e60f16d35bbd07fe9e158309210c
leomet07 <47071760+leomet07@users.noreply.github.com>
2025-07-08 14:31:57 -0400

    Add about page
    Co-authored-by: MaimunaMuntaha <maimunam@stanford.edu>

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,39 @@
 <h1 class="title">About</h1>
+<p>
+    With support from the MTA, CityTech students are collecting temperatures at
+    both the street level and platform level at subway stations in New York City
+    to observe patterns and analyze the <a
+        href="https://www.epa.gov/heatislands">urban heat island's effect</a
+    > on underground subway stations.
+</p>
+<p>
+    Through an analysis of temperatures collected, we will build an interactive
+    dashboard that allows people to access generalized weather information
+    through a geospatial web component.
+</p>
+<p>
+    We are also working on creating a two-tiered forecast model that uses the
+    temperatures collected to train a machine learning model that takes in daily
+    temperatures and dates to predict street level temperatures and predict the
+    temperatures inside of subway stations given the aforementioned analysis of
+    historical subway records.
+</p>
+
+<h2>Abstract</h2>
+<p>
+    New York City faces significant challenges due to the accelerating impacts
+    of climate change. The most affected infrastructures are underground
+    transportation systems, which are particularly vulnerable to the
+    intensifying frequency and duration of heat waves. The fluctuation in
+    temperature within the subway system during these extreme weather events
+    poses serious health and safety risks not only to daily commuters but also
+    transit employees working in these environments. Despite the potential
+    dangers, the extent and dynamics of temperature changes within the subway
+    remain insufficiently understood. To address this knowledge gap, this study
+    conducts a comprehensive investigation into temperature variability across
+    multiple subway stations in NYC, aiming to inform future mitigation
+    strategies and urban planning.
+</p>
+
+<h2>Website Source Code</h2>
+<p><a href="https://github.com/leomet07/subway-heat">Github</a></p>
