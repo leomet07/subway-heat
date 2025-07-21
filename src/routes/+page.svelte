3837b5078b18d0f9704b9b29b85c48c026dbcef4
leomet07 <47071760+leomet07@users.noreply.github.com>
2025-07-21 11:19:00 -0400

    Improve label on title screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 
 <header class="header">
     <hgroup>
-        <h1 class="title">Subway Heat Map</h1>
+        <h1 class="title">Historical Subway Heat Map</h1>
         <p>
             Visit the <a href="/about">about page</a> to learn more about this project.
         </p>
