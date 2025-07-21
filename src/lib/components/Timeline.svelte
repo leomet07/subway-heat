f2df67836e72505e810dc2ee67ea8ae799712d73
leomet07 <47071760+leomet07@users.noreply.github.com>
2025-07-21 11:12:10 -0400

    Improve timeline styles

--- src/lib/components/Timeline.svelte
+++ src/lib/components/Timeline.svelte
@@ -13,21 +13,87 @@
 </script>
 
 <article class="card">
-    {#if station_name}
-        <p>Currently using timeline for {station_name}</p>
-    {:else}
-        <p>Using timeline consisting of all data collection points.</p>
-    {/if}
-    <p>
-        {currentViewInfo.orderedDatesList[currentViewInfo.currentDateIndex]}
-    </p>
-    <div></div>
-    <button onclick={() => currentViewInfo.currentDateIndex--}>Left</button>
-    <button onclick={() => currentViewInfo.currentDateIndex++}>Right</button>
+    <div class="timeline_container">
+        <button
+            class="left"
+            onclick={() => currentViewInfo.currentDateIndex--}
+            disabled={currentViewInfo.currentDateIndex == 0}
+        >
+            <span>&lt;</span>
+        </button>
+        <div class="center">
+            <p>
+                {currentViewInfo.orderedDatesList[
+                    currentViewInfo.currentDateIndex
+                ]}
+            </p>
+        </div>
+        <button
+            class="right"
+            onclick={() => currentViewInfo.currentDateIndex++}
+            disabled={currentViewInfo.currentDateIndex ==
+                currentViewInfo.orderedDatesList.length - 1}
+        >
+            <span>&gt;</span>
+        </button>
+    </div>
+    <div>
+        {#if station_name}
+            <p class="timeline_restriction_label">
+                Currently using timeline for {station_name}.
+            </p>
+        {:else}
+            <p class="timeline_restriction_label">
+                Using timeline consisting of all data collection points.
+            </p>
+        {/if}
+    </div>
 </article>
 
 <style scoped>
-    .card {
-        /* outline: 1px solid red; */
+    .timeline_container {
+        width: 100%;
+        height: 60px;
+        margin-bottom: 0.5rem;
+        display: flex;
+        position: relative;
+    }
+
+    button:active {
+        transform: scale(0.99);
+    }
+    .right,
+    .left {
+        flex: 1;
+        background-color: var(--pico-secondary);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 2.5rem;
+        cursor: pointer;
+        border: none;
+        border-radius: 5px;
+        padding: none;
+    }
+
+    .right:disabled,
+    .left:disabled {
+        background-color: grey;
+    }
+
+    .center {
+        flex: 5;
+        display: grid;
+        place-items: center;
+    }
+
+    .center p {
+        font-size: 2rem;
+    }
+
+    .timeline_restriction_label {
+        font-size: 1.25rem;
+        text-align: center;
+        margin-bottom: 0;
     }
 </style>
