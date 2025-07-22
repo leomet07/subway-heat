611434186aee9e90d77b43bb443c21f7cb6d5922
leomet07 <leomet07@gmail.com>
2025-07-21 23:16:46 -0400

    update lenny about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,144 +1,175 @@
 <h1 class="title">About</h1>
 
 <p>
-    In collaboration with the MTA, CityTech and Eco Rising are analyzing Subway temperatures across New York City MTA stations to observe patterns and analyze the <a
-    href="https://www.epa.gov/heatislands">urban heat island's effect</a
-    > on underground subway stations.
-    CityTech students are collecting temperatures at
-    both the street level and platform level at these subway stations and through an analysis of temperatures collected, we will build an interactive
-    dashboard that allows people to access generalized weather information
-    through a geospatial web component.
+  In collaboration with the MTA, CityTech and Eco Rising are analyzing Subway
+  temperatures across New York City MTA stations to observe patterns and analyze
+  the <a href="https://www.epa.gov/heatislands">urban heat island's effect</a> on
+  underground subway stations. CityTech students are collecting temperatures at both
+  the street level and platform level at these subway stations and through an analysis
+  of temperatures collected, we will build an interactive dashboard that allows people
+  to access generalized weather information through a geospatial web component.
 </p>
 <p>
-    We are also working on creating a two-tiered forecast model that uses the
-    temperatures collected to train a machine learning model that takes in daily
-    temperatures and dates to predict street level temperatures and predict the
-    temperatures inside of subway stations given the aforementioned analysis of
-    historical subway records.
+  We are also working on creating a two-tiered forecast model that uses the
+  temperatures collected to train a machine learning model that takes in daily
+  temperatures and dates to predict street level temperatures and predict the
+  temperatures inside of subway stations given the aforementioned analysis of
+  historical subway records.
 </p>
 
 <h2>Project Description</h2>
 <p>
-    New York City faces significant challenges due to the accelerating impacts
-    of climate change. The most affected infrastructures are underground
-    transportation systems, which are particularly vulnerable to the
-    intensifying frequency and duration of heat waves. The fluctuation in
-    temperature within the subway system during these extreme weather events
-    poses serious health and safety risks not only to daily commuters but also
-    transit employees working in these environments. Despite the potential
-    dangers, the extent and dynamics of temperature changes within the subway
-    remain insufficiently understood. To address this knowledge gap, this study
-    conducts a comprehensive investigation into temperature variability across
-    multiple subway stations in NYC, aiming to inform future mitigation
-    strategies and urban planning.
+  New York City faces significant challenges due to the accelerating impacts of
+  climate change. The most affected infrastructures are underground
+  transportation systems, which are particularly vulnerable to the intensifying
+  frequency and duration of heat waves. The fluctuation in temperature within
+  the subway system during these extreme weather events poses serious health and
+  safety risks not only to daily commuters but also transit employees working in
+  these environments. Despite the potential dangers, the extent and dynamics of
+  temperature changes within the subway remain insufficiently understood. To
+  address this knowledge gap, this study conducts a comprehensive investigation
+  into temperature variability across multiple subway stations in NYC, aiming to
+  inform future mitigation strategies and urban planning.
 </p>
 
 <section>
-    <h2>People</h2>
-  
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-      <div>
-        <img
-            src="/img/bah.png"
-            alt="Dr. Abdou Bah" 
-            style="width:250px; height:250px; object-fit:cover !important;"
-            />
-        <h3>Prof. Abdou Bah</h3>
-        <p>Professor in the Physics department <br /> NYC College of Technology</p>
-        <p>Research interests: Remote sensing, spatial analysis, and climate change.</p>
-      </div>
+  <h2>People</h2>
 
-      <div>
-        <img
-            src="/img/blake.png"
-            alt="Dr. Reginald Blake" 
-            style="width:250px; height:250px; object-fit:cover !important;"
-            />
-        <h3>Dr. Reginald Blake</h3>
-        <p>Associate Provost and Dean of Curriculum and Research<br />NYC College of Technology</p>
-        <p>Research interests: climate modeling, impacts, and urban climate using remote sensing.</p>
-      </div>
-
-      <div>
-        <img
-            src="/img/norouzi.png"
-            alt="Dr. Hamrideza Norouzi" 
-            style="width:250px; height:250px; object-fit:cover !important;"
-            />
-        <h3>Prof. Hamrideza Norouzi</h3>
-        <p>Professor in the Department of Construction Management and Civil Engineering Technology<br />NYC College of Technology</p>
-        <p>Research interests: GIS, remote sensing, earth system sciences, urban climate.</p>
-      </div>
-    </div>
-  
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
     <div>
-      <h3>Undergraduate CCRI Students</h3>
-      <div class="grid md:grid-cols-2 gap-6">
-        <div>
-          <div> 
-            <div>
-                <img
-                src="/img/anon.png"
-                alt="Leonid Metlitsky" 
-                style="width:250px; height:250px; object-fit:cover !important;"
-                />
-              <div>Leonid Metlitsky</div>
-              <div>Rising Freshman at Cornell</div>
-            </div>
-          </div>
-          <p>Major: Environmental Engineering.</p><p> Interests: studying lakes, ... (add more details)</p>
-        </div>
+      <img
+        src="/img/bah.png"
+        alt="Dr. Abdou Bah"
+        style="width:250px; height:250px; object-fit:cover !important;"
+      />
+      <h3>Prof. Abdou Bah</h3>
+      <p>
+        Professor in the Physics department <br /> NYC College of Technology
+      </p>
+      <p>
+        Research interests: Remote sensing, spatial analysis, and climate
+        change.
+      </p>
+    </div>
+
+    <div>
+      <img
+        src="/img/blake.png"
+        alt="Dr. Reginald Blake"
+        style="width:250px; height:250px; object-fit:cover !important;"
+      />
+      <h3>Dr. Reginald Blake</h3>
+      <p>
+        Associate Provost and Dean of Curriculum and Research<br />NYC College
+        of Technology
+      </p>
+      <p>
+        Research interests: climate modeling, impacts, and urban climate using
+        remote sensing.
+      </p>
+    </div>
+
+    <div>
+      <img
+        src="/img/norouzi.png"
+        alt="Dr. Hamrideza Norouzi"
+        style="width:250px; height:250px; object-fit:cover !important;"
+      />
+      <h3>Prof. Hamrideza Norouzi</h3>
+      <p>
+        Professor in the Department of Construction Management and Civil
+        Engineering Technology<br />NYC College of Technology
+      </p>
+      <p>
+        Research interests: GIS, remote sensing, earth system sciences, urban
+        climate.
+      </p>
+    </div>
+  </div>
+
+  <div>
+    <h3>Undergraduate CCRI Students</h3>
+    <div class="grid md:grid-cols-2 gap-6">
+      <div>
         <div>
           <div>
-            <div>
-                <img
-                src="/img/maimuna.png"
-                alt="Maimuna Muntaha" 
-                style="width:250px; height:250px; object-fit:cover !important;"
-                />
-              <div>Maimuna Muntaha</div>
-              <div>Rising Senior at Stanford</div>
-            </div>
+            <img
+              src="/img/lenny.jpg"
+              alt="Lenny Metlitsky"
+              style="width:250px; height:250px; object-fit:cover !important;"
+            />
+            <div>Lenny Metlitsky</div>
+            <div>Rising Freshman at Cornell</div>
           </div>
-          <p>Major: Symbolic Systems (HCI). </p><p>Interests: Remote Sensing, Geospatial Modeling, VR climate simulations.</p>
         </div>
+        <p>Major: Environmental Engineering.</p>
+        <p>
+          Interests: studying lakes, data nerdery, writing, student journalism,
+          tinkering, and cycling
+        </p>
+      </div>
+      <div>
+        <div>
+          <div>
+            <img
+              src="/img/maimuna.png"
+              alt="Maimuna Muntaha"
+              style="width:250px; height:250px; object-fit:cover !important;"
+            />
+            <div>Maimuna Muntaha</div>
+            <div>Rising Senior at Stanford</div>
+          </div>
+        </div>
+        <p>Major: Symbolic Systems (HCI).</p>
+        <p>
+          Interests: Remote Sensing, Geospatial Modeling, VR climate
+          simulations.
+        </p>
       </div>
     </div>
-  
-    <div>
-      <h3>CityTech Students</h3>
-      <p>Most of the data collected for the past year was done by CityTech Students who went into the subways to collect Platform and Street level temperatures and relative humidity.</p>
-      <div class="grid md:grid-cols-3 gap-4">
-        <div> 
-          <p>Damilola BabsOgundeji </p>
-          <p>Malique Paul</p>
-          <p>Anton Nikitin</p>
-          <p>Cesar Vasquez</p>
-          <p>Isaac Lopez</p>
-          </div><div>  
-          <p>Addree Barua</p>
-          <p>Kiran Maharjan</p>
-          <p>Babacar Sarr</p>
-          <p>Tyler Ayala</p>
-          <p>Rean Shahidullah</p>
-        </div>
-        <div> 
-           
-          <p>Kingston Wills Ditsch</p>
-          <p>Kevin Balbuena Montes </p>
-          <p>Shaquan Larose</p>
-          <p>Brigette Alejandro</p>
-          <p>Hailah Nagi </p>
-        </div> 
+  </div>
+
+  <div>
+    <h3>CityTech Students</h3>
+    <p>
+      Most of the data collected for the past year was done by CityTech Students
+      who went into the subways to collect Platform and Street level
+      temperatures and relative humidity.
+    </p>
+    <div class="grid md:grid-cols-3 gap-4">
+      <div>
+        <p>Damilola BabsOgundeji</p>
+        <p>Malique Paul</p>
+        <p>Anton Nikitin</p>
+        <p>Cesar Vasquez</p>
+        <p>Isaac Lopez</p>
+      </div>
+      <div>
+        <p>Addree Barua</p>
+        <p>Kiran Maharjan</p>
+        <p>Babacar Sarr</p>
+        <p>Tyler Ayala</p>
+        <p>Rean Shahidullah</p>
+      </div>
+      <div>
+        <p>Kingston Wills Ditsch</p>
+        <p>Kevin Balbuena Montes</p>
+        <p>Shaquan Larose</p>
+        <p>Brigette Alejandro</p>
+        <p>Hailah Nagi</p>
+      </div>
     </div>
-  
+
     <div>
       <h3>INSPIRE Program Contributors</h3>
       <div>
-        Thank you to everyone involved in the INSPIRE program; high school students learned how to use FLIR thermal cameras and collected temperatures out in the field for a day.
+        Thank you to everyone involved in the INSPIRE program; high school
+        students learned how to use FLIR thermal cameras and collected
+        temperatures out in the field for a day.
       </div>
     </div>
-  </section>
- 
+  </div>
+</section>
+
 <h2>Website Source Code</h2>
 <p><a href="https://github.com/leomet07/subway-heat">Github</a></p>
